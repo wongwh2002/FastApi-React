886ecfe92c7ec37c72913ac19c26cbe302221ee4
wongwh2002 <wenghongwong5686@gmail.com>
2025-12-16 12:37:54 +0800

    first init at 19:26

---
+++ README.md
@@ -0,0 +1,2 @@
+Learning Langchain with react and FastAPI
+Following Tim's tutorial on https://www.youtube.com/watch?v=_1P0Uqk50Ps
---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="Choose Your Own Adventure Game API",
+    description="API to generate cool stories",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+#cross origin resource sharing
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
