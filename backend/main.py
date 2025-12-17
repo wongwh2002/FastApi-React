6fe288b01b9f40d0994802cf679ffa2a1b89232b
wongwh2002 <wenghongwong5686@gmail.com>
2025-12-17 21:49:45 +0800

    add base story and tables and initial main config, 42:28

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from core.config import settings
+
 app = FastAPI(
     title="Choose Your Own Adventure Game API",
     description="API to generate cool stories",
@@ -12,8 +14,12 @@ app = FastAPI(
 #cross origin resource sharing
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
